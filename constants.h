42ee92fdb3ca169263591597e474372978d296a8
Patrick Colp <pjcolp@cs.ubc.ca>
2013-05-29 22:28:08 -0700

    Forgot to add constants.h

---
+++ constants.h
@@ -0,0 +1,34 @@
+/******************************************************************************
+ * Copyright (c) 2013 Patrick Colp
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#define TEMPERATURE_PRECISION  (9)
+
+#define SENSOR_TIME    (1000)
+#define BLINK_TIME     (500)
+#define BEEP_TIME      (500)
+#define TIMER_TIME     (1000) // (1000*60) // 1 minute
+#define REMINDER_TIME  (1000*10) // 10 seconds
+
+#define ELEMENT_CONTROL_RIMS  (false)
+#define ELEMENT_CONTROL_BK    (true)
+
+#endif
+
